ff018977557653e032bc247b7de40a833211d31e
Yerkanat <sayranbekov.0000@gmail.com>
2024-03-24 23:42:49 +0500

    first commit

---
+++ app/components/CountDown.jsx
@@ -0,0 +1,79 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+function CountDown() {
+
+    const formatNumber = (number) => {
+        return number < 10 ? `0${number}` : number;
+    };
+
+    const calculateTimeLeft = () => {
+        const difference = + new Date('2024-06-22T00:00:00') - + new Date();
+        let timeLeft = {};
+    
+        if (difference > 0) {
+          timeLeft = {
+            weeks: formatNumber(Math.floor(difference / (1000 * 60 * 60 * 24 * 7))),
+            days: formatNumber(Math.floor((difference / (1000 * 60 * 60 * 24)) % 7)),
+            hours:  formatNumber(Math.floor((difference / (1000 * 60 * 60)) % 24)),
+            minutes:  formatNumber(Math.floor((difference / 1000 / 60) % 60)),
+            seconds:  formatNumber(Math.floor((difference / 1000) % 60)),
+          };
+        }
+    
+        return timeLeft;
+    };
+
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+    
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setTimeLeft(calculateTimeLeft());
+        }, 1000);
+      
+        return () => clearTimeout(timer);
+    }, [timeLeft]);
+      
+    
+    const { weeks, days, hours, minutes, seconds } = timeLeft;
+    
+    
+    return (
+        <div className="grid grid-cols-5 sm:text-lg font-['wg'] bg-[rgba(255,255,255,0.3)] p-3 rounded-lg">
+            <div className="col-span-1 text-center text-2xl">
+                {weeks}
+            </div>
+            <div className="col-span-1 text-center text-2xl">
+                {days}
+            </div>
+            <div className="col-span-1 text-center text-2xl">
+                {hours}
+            </div>
+            <div className="col-span-1 text-center text-2xl">
+                {minutes}
+            </div>
+            <div className="col-span-1 text-center text-2xl">
+                {seconds}
+            </div>
+            <div className="col-span-1 text-center">
+                {weeks === 1 ? 'Неделя' : 'Недель'}
+            </div>
+            <div className="col-span-1 text-center">
+                {days === 1 ? 'День' : 'Дней'}
+            </div>
+            <div className="col-span-1 text-center">
+                {hours === 1 ? 'Час' : 'Часов'}
+            </div>
+            <div className="col-span-1 text-center">
+                {minutes === 1 ? 'Минута' : 'Минут'}
+            </div>
+            <div className="col-span-1 text-center">
+                {seconds === 1 ? 'Секунда' : 'Секунд'}
+            </div>
+        </div>
+    );
+}
+
+export default CountDown;
--- app/page.js
+++ app/page.js
@@ -1,113 +1,135 @@
-import Image from "next/image";
+'use client'
+import dynamic from 'next/dynamic';
+import { YMaps, Map, Button, Placemark } from '@pbe/react-yandex-maps';
+import { useState } from 'react';
+import localFont from 'next/font/local';
 
+const wg = localFont({
+  src: [
+    {
+      path: '../public/WonderGardenScript-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-wg',
+})
+
+const CountDown = dynamic(() => import('./components/CountDown'), {
+  ssr: false
+})
 export default function Home() {
+
+  const [name, setName] = useState('')
+  const [status, setStatus] = useState('')
+
+  const [isConfetti, setIsConfetti] = useState(false)
+
+  const Confetties = () => {
+    setStatus('ИӘ, КЕЛЕМІН')
+    setIsConfetti(true)
+  }
+
+  const Vote = async () => {
+    try {
+      const response = await fetch('https://sheetdb.io/api/v1/zu4i3u7qnij0h', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          data: [
+            {
+              'ФИО': name,
+              'Статус': status,
+            }
+          ]
+        })
+      });
+
+      const data = await response.json();
+      console.log(data); // Check response data
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  <div className='font-[wg] text-center'>
+    <main className='flex flex-col items-center justify-between relative'>
+      <img src='/hero.jpg' alt='Hero' className='w-full'/>
+    
+      <img src='/stars.png' alt='stars' className=' absolute'/>
+      <div className='flex flex-col absolute top-0 w-[80%] justify-between h-[110%]'>
+        <div className='text-left relative'>
+          <p className={`${wg.className} text-[60px] m-0 absolute top-0`}>Бекзат and Венера</p>
+          <p className=' absolute top-16'>Приглашаем на <br /> свадьбу <br />22.06.2024</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <CountDown />
+        <div className=' text-center  bg-[rgba(255,255,255,0.3)] p-3 rounded-lg'>
+          <h1 className='text-xl'>Құрметті</h1>
+          <p className='text-sm'>ағайын-туыс, құда-жекжат, дос жарандар, ұлымыз бен келініміз өз бақытын тапты, сол бақытың куәсі болуға шақырамыз!</p>
+          <p>22.06.2024 | 17:00 | сенбі</p>
+        </div>
+        
       </div>
     </main>
+
+    <div className='relative flex justify-center mb-[400px]'>
+        <img src='/3.jpeg' alt='Section' className='w-auto mx-auto mt-32 rounded-lg absolute rotate-[25deg] top-10 right-10 h-[200px]'/>
+        <img src='/2.jpeg' alt='Section' className='w-auto mx-auto mt-32 rounded-lg absolute -rotate-[25deg] top-10 left-10 h-[200px]'/>
+        <img src='/1.jpeg' alt='Section' className='w-auto mx-auto mt-32 rounded-lg absolute h-[250px] '/>
+    </div>
+
+    <div className='w-[80%] mx-auto text-center relative mb-[200px] border-white border-[1px] '>
+        <p className={`${wg.className} text-[60px] m-0 absolute top-0 right-10`}>Бекзатпен Венеранын </p>
+        <p className=' absolute top-16'>отау құру тоына арналған салтанатты ақ дастарханымыздың қадірлі қонағы болуға шақырамыз!</p>
+    </div>
+
+    <div className='w-[80%] mx-auto mb-16'>
+      <h3 className=' font-extrabold text-2xl'>Dress Code | Black Tie</h3>
+      <div className=' flex justify-evenly w-[80%] mx-auto py-2'>
+        <div className='h-[25px] w-[25px] rounded-full bg-[#dddccb]'></div>
+        <div className='h-[25px] w-[25px] rounded-full bg-[#b8b0a0]'></div>
+        <div className='h-[25px] w-[25px] rounded-full bg-[#96825f]'></div>
+        <div className='h-[25px] w-[25px] rounded-full bg-[#181818]'></div>
+      </div>
+      <p>Біздің тойға сәйкес киім үлгісін таңдап, айтулы күннің dress code -ын ұстануыңызды сұраймыз!</p>
+    </div>
+
+
+    <div className=' flex flex-col w-[80%] mx-auto mb-[100px] bg-[rgba(255,255,255,0.3)] p-4 rounded-lg'>
+      <p className=' text-xl py-4'>Тойға қатысуыңызды <br /> растауыңызды сұраймыз!</p>
+      <input type="text" id="first_name" class=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-4" placeholder="ФИО" required onChange={(e) => setName(e.target.value)} />
+      <div className=' flex flex-col gap-6 justify-between'>
+        <div className='flex items-center'>
+            <input id='default-radio-1' type='radio' value='ИӘ, КЕЛЕМІН' name='default-radio' className='w-4 h-4 outline-none text-blue-600 bg-gray-100 border-gray-300' onChange={(e) => setStatus(e.target.value)}/>
+            <label for='default-radio-1' className='ms-2 text-xs font-medium text-white '>ИӘ, КЕЛЕМІН</label>
+        </div>
+        <div className='flex items-center'>
+            <input id='default-radio-2' type='radio' value='ӨКІНІШКЕ ОРАЙ КЕЛЕ АЛМАЙМЫН' name='default-radio' className='w-4 h-4 outline-none text-blue-600 bg-gray-100 border-gray-300' onChange={(e) => setStatus(e.target.value)}/>
+            <label for='default-radio-2' className='ms-2 text-xs font-medium text-white '>ӨКІНІШКЕ ОРАЙ КЕЛЕ АЛМАЙМЫН</label>
+        </div>
+      </div>
+      <button type='submit' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' onClick={Vote}>Растау</button>
+    </div>
+
+    <YMaps query={{lang: 'ru_RU', apikey: '536168cc-8dbb-4923-a06f-9a6bd5a9cf15'}}>
+      <Map defaultState={{ center: [51.14499045698973, 71.37709503819221], zoom: 19 }} width='100%' height='400px' >
+        <Placemark geometry={[51.14499045698973, 71.3772]} options={{ fillColor: '#f00' }} />
+        <Button
+          options={{ maxWidth: 128 }}
+          data={{ content: 'Заказать Такси' }}
+          defaultState={{ selected: true }}
+          onClick={(e) => {
+            window.location.href = 'https://3.redirect.appmetrica.yandex.com/route?utm_source=yamaps&utm_medium=api&appmetrica_tracking_id=241755468559577482&ref=2334695&domain=ru&lang=ru&start-lat=&start-lon=&end-lat=51.14501&end-lon=71.377198'
+          }}
+          />
+      </Map>
+    </YMaps>
+  </div>
+  
   );
 }
